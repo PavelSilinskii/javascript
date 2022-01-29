ddd9d5aa86211f91037ba6543199ed0c60c2356c
Pavel <work322@gmail.com>
2022-01-29 09:22:07 +0200

    приоритет операций javascript

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-var num1 = 12
-var num2 = 8
+var result = 12 - 6 / 3
+var result2 = 3 + 4 * 2
+var isGreater = 20 - 6 * 3 >= 4
+var isGreater2 = 20 - 6 * 3 >= 1
 
-console.log('+', num1+num2)
-console.log('-', num1-num2)
-console.log('*', num1*num2)
-console.log('/', num1/num2)
+console.log('12 - 6 / 3:',result)
+console.log('3 + 4 * 2:',result2)
+console.log('20 - 6 * 3 >= 4:',isGreater)
+console.log('20 - 6 * 3 >= 1:',isGreater2)
 
-var str1 = 'hello'
-var str2 = 'world'
-
-console.log('+', str1 + ' ' + str2)
-
-console.log('1+2:', 1 + str2)
-
-console.log('str1+num1:',str1+num1)
-console.log('Boolean + string', true + str1)
-
-console.log('True + 1:', true + 1)
-console.log('False +1:', false +1)
