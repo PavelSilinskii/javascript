6012d384bfed1cc274c64ebced814e36e0ebb1f2
Pavel <work322@gmail.com>
2022-01-29 09:10:17 +0200

    базовые операции javascript

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+var num1 = 12
+var num2 = 8
+
+console.log('+', num1+num2)
+console.log('-', num1-num2)
+console.log('*', num1*num2)
+console.log('/', num1/num2)
+
+var str1 = 'hello'
+var str2 = 'world'
+
+console.log('+', str1 + ' ' + str2)
+
+console.log('1+2:', 1 + str2)
+
+console.log('str1+num1:',str1+num1)
+console.log('Boolean + string', true + str1)
+
+console.log('True + 1:', true + 1)
+console.log('False +1:', false +1)
