5fb5b325e52a44a3963a14e2ffca178e53c7e5ed
Pavel <work322@gmail.com>
2022-01-31 20:17:58 +0200

    Видимость функций в документе

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-var carName = 'Ford'
-var carYear = 2013
-var personYear =  1990
-
-function calculateAge(year){
-   var currentYear= 2022
-   var result = currentYear - year
-   return result
+var sayHelloTo = function (name){
+    console.log('Привет, ' + name)
 }
 
-function checkAndLogAge(year,name,compareTo){
-    if(calculateAge(year)<compareTo){
-        console.log('Возраст ' +  name + ' меньше ' + compareTo + ' лет')
-    }else{
-        console.log('Возраст ' +  name + ' больше ' + compareTo + ' лет')
-    }
-}
+sayHelloTo('Ирина')
 
-checkAndLogAge(carYear,'машины',8)
-checkAndLogAge(personYear,'человека',32)
+sayHello('Игорь')
+
+ function sayHello(name){
+    console.log('Привет, ' + name)
+ }
