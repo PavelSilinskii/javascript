00c07d270a76876460dc75d776f95eb82ddb730e
Pavel <work322@gmail.com>
2022-01-31 20:10:41 +0200

    Написание функции для определения различий

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
 var carName = 'Ford'
-var carYear = 2008
-var personYear = 1990
+var carYear = 2013
+var personYear =  1990
 
 function calculateAge(year){
-  var currentYear = 2022
-  var result = currentYear - year
-  return result
+   var currentYear= 2022
+   var result = currentYear - year
+   return result
 }
 
-function checkAndLogAge(year){
-    if(calculateAge(year)<10){
-        console.log('Возраст меньше 10 лет')
+function checkAndLogAge(year,name,compareTo){
+    if(calculateAge(year)<compareTo){
+        console.log('Возраст ' +  name + ' меньше ' + compareTo + ' лет')
     }else{
-        console.log('Возраст больше 10 лет')
-    }   
+        console.log('Возраст ' +  name + ' больше ' + compareTo + ' лет')
+    }
 }
 
-checkAndLogAge(carYear)
-checkAndLogAge(personYear)
-
-
-// if(calculateAge(carYear)<10){
-//     console.log('Возраст меньше 10 лет')
-// }else{
-//     console.log('Возраст больше 10 лет')
-// }
-
-// if((2022 - carYear)<10){
-//     console.log('Возраст меньше 10 лет')
-// }else{
-//     console.log('Возраст больше 10 лет')
-// }
-
-// if(calculateAge(personYear)<10){
-//     console.log('Возраст меньше 10 лет')
-// }else{
-//     console.log('Возраст больше 10 лет')
-// }
-
-
-// if((2022 - carYear)<10){
-//     console.log('Возраст меньше 10 лет')
-// }else{
-//     console.log('Возраст больше 10 лет')
-// }
-
-// if((2022 - personYear)<10){
-//     console.log('Возраст меньше 10 лет')
-// }else{
-//     console.log('Возраст больше 10 лет')
-// }
-
+checkAndLogAge(carYear,'машины',8)
+checkAndLogAge(personYear,'человека',32)
