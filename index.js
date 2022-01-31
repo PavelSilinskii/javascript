661c72af502fb7c2f1d5114a1d4abe088fd4c517
Pavel <work322@gmail.com>
2022-01-31 20:39:18 +0200

    Работа со строками

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-var sayHelloTo = function (name){
-    console.log('Привет, ' + name)
-}
+var str1 = 'Hello world' // предпочтительная запись
+var str2 = "Hello world2"
 
-sayHelloTo('Ирина')
+var personName = 'Виктор'
+var message='Человека зовут "'+ personName +'"'
+var message2="Человека зовут '"+ personName +"'"
+var message3='Человека зовут \''+ personName +'\''
 
-sayHello('Игорь')
+console.log(str1)
+console.log(str2)
+console.log(message)
+console.log(message2)
+console.log(message3)
 
- function sayHello(name){
-    console.log('Привет, ' + name)
- }
+var newMessage = 'Hello world!!!'
+
+console.log(newMessage.length)
+console.log(newMessage.toUpperCase())
+console.log(newMessage.toLowerCase())
+console.log(newMessage.charAt(1))
+console.log(newMessage.indexOf('world'))
+console.log(newMessage.indexOf('cat'))
+console.log(newMessage.substr(1,4))
+console.log(newMessage.substr(newMessage.indexOf('world'),5))
+console.log(newMessage.substring(1,3))
