4d678f079f746a4e18e38e24dd22c78d9e2368aa
Pavel <work322@gmail.com>
2022-01-31 17:23:21 +0200

    Как работают логические операторы

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-console.log('5 > 3', 5 > 3)
-console.log('3 < 2', 3 < 2 )
+/*
 
-console.log('8 >= 7', 8 >= 7)
-console.log('12 <= 12', 12 <= 12)
-console.log('12 < 12', 12 < 12)
 
-console.log('4 == 4', 4 == 4)
-console.log('4 == 5', 4 == 5)
+&&(и) -  / true если все значения true
+||(или) -  / true если хоть одно значения true
+! (нет) - инвертирует true в false
+*/ 
 
-console.log('4 != 4', 4 != 4)
-console.log('4 != 5', 4 != 5)
+console.log('true && true',true && true)
+console.log('true && false',true && false)
 
-console.log('4 === 4', 4 === 4)
-console.log('4 !== string 4', 4 !== '4')
-console.log('4 === string 4', 4 === '4')
-console.log('4 == string 4', 4 == '4')
+console.log('true || false',true || false)
+console.log('false || false',false || false)
+
+console.log('!false',!false)
+console.log('!true',!true)
+
+console.log('!!true',!!true)
+
+console.log('(false && true) || (true || false) || !true',(false && true) || (true || false) || !true)
+// false || true || false => true
