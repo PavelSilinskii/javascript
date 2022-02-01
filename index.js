d05b4deaa480ac4a88e8760edbcd2e95564da752
Pavel <work322@gmail.com>
2022-02-02 00:31:55 +0300

    Получение и изменение атрибутов

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-// var div = document.getElementById('playground')
-// var p =document.getElementsByClassName('text')
-// var h1= document.getElementsByTagName('h1')
-// console.log(div)
-// console.log(p)
-// console.log(h1)
+var a = document.querySelector('a')
+var oldHref = a.getAttribute('href')
+var oldTitle = a.getAttribute('title')
 
-var div = document.querySelector('#playground')
-var p = document.querySelectorAll('.text')
-var h1= document.querySelector('h1')
-var input = document.querySelector('input')
-div.innerHTML='<h2 style="color:red;">From javascript</h2>'
-h1.textContent = 'Change from JS' 
-console.log(input.value)
-console.log(div.innerHTML)
-console.log(p)
-console.log(h1.textContent)
-
+a.setAttribute('href','https://yandex.ru')
+a.setAttribute('title','Go Yandex')
+a.textContent = 'Yandex'
+console.log(a.attributes)
