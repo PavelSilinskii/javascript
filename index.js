b1a4df0b7bd2d8ada2dbc0285fabb7f84eccb193
Pavel <work322@gmail.com>
2022-02-03 23:42:29 +0300

    Классы в javascript

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ var oldTitle = a.getAttribute('title')
 a.setAttribute('href','https://yandex.ru')
 a.setAttribute('title','Go Yandex')
 a.textContent = 'Yandex'
-console.log(a.attributes)
+
+var box1 =document.querySelector('#box1')
+var box2 =document.querySelector('#box2')
+
+box1.classList.add('red')
+
+var hasClass = box2.classList.contains('blue')
+console.log(hasClass)
+
+if(hasClass){
+    box2.classList.remove('blue')
+}else{
+    box2.classList.add('blue')
+}
