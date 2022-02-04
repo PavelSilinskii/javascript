837555b728123029ed5b7171703cf35b02839a3c
Pavel <work322@gmail.com>
2022-02-05 00:15:35 +0300

    Встроеные команды для взаимодействия с пользователем

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-//var p =document.querySelectorAll('p')
-// for(var i = 0; i < p.length; i++){
-//     p[i].addEventListener('click', function(event){
-//         event.target.style.color ='blue'
-//     })
-// }
+document.querySelector('#alert').addEventListener('click',function(){
+    alert('Вы успешно кликнули по кнопке!')
+})
 
-document.getElementById('wrapper').addEventListener('click',function(event){
-
-    var tagName = event.target.tagName.toLowerCase()
-
-    if(tagName==='p'){
-        event.target.style.color = 'blue'
-    }
-
-    if(event.target.classList.contains('color')){
-        event.target.style.color = 'red'
+document.querySelector('#confirm').addEventListener('click',function(){
+   var decision = confirm('Вы уверены в том что хотите нажать на кнопку!')
+   if(decision){ 
+       alert('Вы успешно кликнули по кнопке!')
     }
 })
 
+document.querySelector('#prompt').addEventListener('click',function(){
+    var age = prompt('Введите свой возраст!', 18)
+
+    if(age>18){
+        alert('Вы можете пройти!')
+    }else{
+        alert('Вы еще слишком молоды!')
+    }
+})
+
+console.log('console.log')
+console.warn('console.warn')
+console.info('console.info')
+console.error('console.error') //не останавливает скрипт
+
+throw new Error('Error') //выкидываеи сообщение и останаливает скрипт
