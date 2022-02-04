6e16fdc69f444c23941383f58abd878964b32f02
Pavel <work322@gmail.com>
2022-02-05 01:15:56 +0300

    Работа с датой в Javascript

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var person = {
-    name : 'Max',
-    age : 26,
-    car:{
-        model: 'Ford'
-    },
-    job: 'FrontEnd',
-    friends: ['Elena', 'Igor']
-}
-
-var str = JSON.stringify(person) // преобразование в строку
-console.log(str)
-
-var obj2 =JSON.parse(str) //преобразование в объект
-console.log(obj2)
+var date = new Date()
+console.log(date)
+//console.log(date.getTimezoneOffset())
+console.log(date.setDate(10))
+console.log(date.getDate())
+console.log(date.getTime()) //timestamp
+console.log(Date.now())
+var date2 = new Date( 360 * 24 * 3)
+console.log(date2)
