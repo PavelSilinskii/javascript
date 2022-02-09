15d31d34d2611251119bdc72daf78416004e6142
Pavel <work322@gmail.com>
2022-02-10 00:19:39 +0300

    Введение в объекты и прототипы

--- index.js
+++ index.js
@@ -1,110 +1,18 @@
-var $start = document.querySelector('#start')
-var $game = document.querySelector('#game')
-var $time =document.querySelector('#time')
-var $result = document.querySelector('#result')
-var $timeHeader = document.querySelector('#time-header')
-var $resultHeader = document.querySelector('#result-header')
-var $gameTime = document.querySelector('#game-time')
+//number, string, boolean, null, undefined
+//object
 
-var colors = ['red','blue','green','yellow','black','pink']
-var score = 0
-var isGameStarted = false
+var car = {
+    name:'Ford',
+    year:2015,
+    person:{
+        name:'Egor',
+        year:2016
 
-$start.addEventListener('click',startGame)
-$game.addEventListener('click',handleBoxClick)
-$gameTime.addEventListener('input', setGameTime)
-
-function show ($el){
-$el.classList.remove('hide')
-}
-function hide ($el){
-$el.classList.add('hide')
+    }
 }
 
+// car.__proto__ => Object.prototype
 
+//[] => Array =>Object
 
-function startGame(){
-    score = 0
-    
-    setGameTime()
-    $gameTime.setAttribute('disabled','true')
-    isGameStarted = true
-    $game.style.backgroundColor = 'white'
-    $start.classList.add('hide')
-
-    var interval = setInterval(function(){
-        var time = parseFloat($time.textContent)
-       if(time <=0){
-
-           clearInterval(interval)
-           endGame()
-
-       }else{
-           $time.textContent = (time - 0.1).toFixed(1)
-       }
-        
-    },100)
-
-    renderBox()
-}
-
-function setGameScore(){
-    $result.textContent = score.toString()
-}
-
-function setGameTime(){
-    var time = parseInt($gameTime.value)
-    $time.textContent = time.toFixed(1)
-    show($timeHeader)
-    hide($resultHeader)
-}
-
-function endGame(){
-isGameStarted = false
-setGameScore()
-$start.classList.remove('hide')
-$game.innerHTML = ''
-$game.style.backgroundColor = '#ccc'
-$timeHeader.classList.add('hide')
-$resultHeader.classList.remove('hide')
-$gameTime.removeAttribute('disabled')
-}
-
-
-function handleBoxClick(event){
-if (!isGameStarted){
-    return 
-}
-
-if(event.target.dataset.box){
-score++
-renderBox()
-}
-}
-
-function renderBox(){
-$game.innerHTML = '' //очистка поля
-var box = document.createElement('div')// создание элемента
-var boxSize = getRandom(30,100)
-var gameSize = $game.getBoundingClientRect() //Значение размеров обасти
-var maxTop = gameSize.height - boxSize
-var maxLeft = gameSize.width - boxSize
-var randColor = getRandom(100,999)
-var randColorIndex = getRandom(0,colors.length)
-
-
-box.style.height = box.style.width = boxSize + 'px'
-box.style.position = 'absolute'
-box.style.backgroundColor=colors[randColorIndex]
-box.style.top = getRandom(0,maxTop) + 'px'
-box.style.left = getRandom(0,maxLeft) + 'px'
-box.style.cursor = 'pointer'
-box.setAttribute('data-box','true')
-
-$game.insertAdjacentElement('afterbegin',box)
-
-}
-
-function getRandom(min,max){
-    return Math.floor(Math.random()*(max-min)+min)
-}
+console.log(car)
