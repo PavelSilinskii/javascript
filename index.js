1bd30b51c2b2066535d67230d99c3e79d6f7ceb7
Pavel <work322@gmail.com>
2022-02-12 01:51:38 +0300

    Замыкания для объектов

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-var person = {
-    name:'Max',
-    age: 28,
-    job: 'Frontend'
-}
-
-
-
-
-// for (var key in person){
-//     if(person.hasOwnProperty(key))
-//     console.log(person[key])
+// var createCounter = function(counterName){
+//    var counter = 0
+ 
+//     return function(){
+//         console.log(counterName, ++counter)
+//     }
 // }
 
-var keys= Object.keys(person).forEach(function(key){
-    console.log(person[key])
-})
+// var counterA = createCounter('Counter A')
+// var counterB = createCounter('Counter B')
 
-// console.log(keys)
+// console.log(counterA())
+// console.log(counterA())
+// console.log(counterB())
+// console.log(counterA())
+// console.log(counterB())
+
+var createCounter = function(counterName){
+    var counter = 0
+  
+     return {
+         incriment: function(){
+             counter++
+         },
+         decrement: function(){
+             counter--
+         },
+         getCounter: function(){
+             return counter
+         }
+     }
+ }
+
+var counterA = createCounter('Counter A')
+var counterB = createCounter('Counter B')
+
+counterA.incriment()
+counterA.incriment()
+counterA.incriment()
+
+counterB.decrement()
+counterB.decrement()
+
+console.log(counterA.getCounter())
+console.log(counterB.getCounter())
