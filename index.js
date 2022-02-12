f71f72ea4db687de9d09f76bb98a713b85fdd8b4
Pavel <work322@gmail.com>
2022-02-13 01:34:11 +0300

    Задание параметров по умолчанию для функци

--- index.js
+++ index.js
@@ -1,46 +1,12 @@
-// function getAge(year){
-//     const current = new Date().getFullYear()
-//     return current - year
-// }
-
-// console.log(getAge(1983))
-
-// const calculateAge = (year) => {
-//     const current = new Date().getFullYear()
-//      return current - year
-// }
-//  console.log(calculateAge(1981))
-
-// const getAge = year =>{
-//      const current = new Date().getFullYear()
-//      return current - year   
-// }
-// console.log(getAge(1979))
-
-// const getAge = year => new Date().getFullYear() - year   
-// console.log(getAge(2002))
-
-// const logAge = year => console.log(new Date().getFullYear() - year) 
-// logAge(2016)
-
-
-// const person = {
-//     age: 25,
-//     firstName: 'Maxim',
-//     logNameWithTimeout:function(){
-//         setTimeout(function(){
-//             console.log(this.firstName)
-//         }.bind(this),1000)
-//     }
-// }
-
-const person = {
-    age: 25,
-    firstName: 'Maxim',
-    logNameWithTimeout(){
-        setTimeout(()=>{
-            console.log(this.firstName)
-        },1000)
-    }
+const createPost = (title,text = 'Default text',date = new Date().toLocaleDateString()) =>{
+   // date = date || new Date().toLocaleDateString()
+return{
+   title, //если равны ключ и значение
+   text1: text,
+   date 
 }
-person.logNameWithTimeout()
+}
+
+const post = createPost('Скоро новый год!')
+
+console.log(post)
