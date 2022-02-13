1d8c1e19a32cc40c26646276084c65b10a5a259b
Pavel <work322@gmail.com>
2022-02-13 23:33:53 +0300

    объекты в ES6

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const createPost = (title,text = 'Default text',date = new Date().toLocaleDateString()) =>{
-   // date = date || new Date().toLocaleDateString()
-return{
-   title, //если равны ключ и значение
-   text1: text,
-   date 
-}
+// const createCar = (name,model) => {
+//    return{name,model}
+// }
+
+const createCar = (name,model) => ({name,model})
+
+
+const ford = createCar('Ford','Focus')
+
+console.log(ford)
+
+const yearField = 'year' 
+
+const bmw = {
+   name: 'BMW',
+   ['model']: 'X6 Sport',
+   [yearField]: 2018,
+   // logFields(){
+   //    console.log(this.name, this.model, this.year)
+   // }
+   //новый вид записи
+   logFields(){
+      const{name,model,year} = this
+      console.log(name, model, year)
+   }
+
 }
 
-const post = createPost('Скоро новый год!')
+console.log(bmw)
+bmw.logFields()
 
-console.log(post)
+//const year = bmw.year
+const {year} = bmw
+console.log(year)
