d9526d5bfc0ed1cf047daefc479b04a8a38d0430
Pavel <work322@gmail.com>
2022-02-18 00:20:37 +0300

    select созданный через javascript

--- index.js
+++ index.js
@@ -1,67 +1,56 @@
-//RootElement <=Box <= instance
+class Dropdown{
+   constructor(selector,options){
+      this.$el =document.querySelector(selector)
+      this.items = options.items
+    
 
-class RootElement{
-   constructor(tagName = 'div'){
-      this.$el = document.createElement(tagName)
-      this.$el.style.marginBottom = '20px'
+      this.$el.querySelector('.dropdown__label').textContent = this.items[4].label
+
+      this.$el.addEventListener('click',event => {
+         if(event.target.classList.contains('dropdown__label')){
+            
+            if(this.$el.classList.contains('open')){
+               this.close()
+            }else{
+               this.open()
+            }
+
+         }else if(event.target.tagName.toLowerCase() === 'li'){
+            this.select(event.target.dataset.id)
+         }
+      })
+
+      const itemsHTML = this.items.map(i => {
+         return `<li data-id="${i.id}">${i.label}</li>`
+      }).join(' ')
+
+      this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML)
    }
 
-   hide(){
-      this.$el.style.opacity = '0'
+   select (id){
+      const item = this.items.find(i => i.id === id)
+      this.$el.querySelector('.dropdown__label').textContent = item.label
+      this.close()
    }
-   append(){
-      document.querySelector('.wrapper').insertAdjacentElement('beforeend',this.$el)
+
+
+   open(){
+      this.$el.classList.add('open')
    }
-   show(){
-      this.$el.style.opacity = '1'
-   }
-}
 
-class Box extends RootElement{
-constructor(color, size = 150, tagName){
-super(tagName)
-this.color = color
-this.size = size
-}
-
-create(){
-  console.log(this.$el)
-  this.$el.style.background = this.color
-  this.$el.style.width = this.$el.style.height = `${this.size}px`
-
-//   document.querySelector('.wrapper').insertAdjacentElement('afterbegin',this.$el)
-this.append()
-  return this
-}
-}
-
-
-class Circle extends RootElement {
-   constructor(color){
-      super()
-      this.color = color
-   }
-   create(){
-      this.$el.style.borderRadius = '50%'
-      this.$el.style.width = this.$el.style.height = `120px`
-      this.$el.style.background = this.color
-      // document.querySelector('.wrapper').insertAdjacentElement('afterbegin',this.$el)
-      this.append()
-      return this
+   close(){
+      this.$el.classList.remove('open')
    }
 }
 
-const redBox = new Box('red', 100, 'div').create()
-const blueBox = new Box('blue', 150, 'div').create()
 
-// blueBox.hide()
 
-const circle = new Circle('green').create()
-
-circle.$el.addEventListener('mouseenter',()=>{
-   circle.hide()
-})
-
-circle.$el.addEventListener('mouseleave',()=>{
-   circle.show()
+const dropdown = new Dropdown('#dropdown',{
+   items:[
+      {label:'Москва',id: 'msk'},
+      {label:'Санкт-Петербург',id: 'spb'},
+      {label:'Новосибирск',id: 'nsk'},
+      {label:'Краснодар',id: 'krdr'},
+      {label:'Калининград',id: 'kld'},
+   ]
 })
