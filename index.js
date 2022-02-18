19c2a27b3c5734127eaa6275168e0b850c7e4cf3
Pavel <work322@gmail.com>
2022-02-19 00:07:47 +0300

    Интервалы и таймеры

--- index.js
+++ index.js
@@ -1,56 +1,12 @@
-class Dropdown{
-   constructor(selector,options){
-      this.$el =document.querySelector(selector)
-      this.items = options.items
-    
+// setTimeout(function(){
+//    alert('hello timeout')
+// }, 2*1000)
 
-      this.$el.querySelector('.dropdown__label').textContent = this.items[4].label
+var counter = 0
+var interval = setInterval(function(){
+console.log (++counter)
+}, 1000*2)
 
-      this.$el.addEventListener('click',event => {
-         if(event.target.classList.contains('dropdown__label')){
-            
-            if(this.$el.classList.contains('open')){
-               this.close()
-            }else{
-               this.open()
-            }
-
-         }else if(event.target.tagName.toLowerCase() === 'li'){
-            this.select(event.target.dataset.id)
-         }
-      })
-
-      const itemsHTML = this.items.map(i => {
-         return `<li data-id="${i.id}">${i.label}</li>`
-      }).join(' ')
-
-      this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML)
-   }
-
-   select (id){
-      const item = this.items.find(i => i.id === id)
-      this.$el.querySelector('.dropdown__label').textContent = item.label
-      this.close()
-   }
-
-
-   open(){
-      this.$el.classList.add('open')
-   }
-
-   close(){
-      this.$el.classList.remove('open')
-   }
-}
-
-
-
-const dropdown = new Dropdown('#dropdown',{
-   items:[
-      {label:'Москва',id: 'msk'},
-      {label:'Санкт-Петербург',id: 'spb'},
-      {label:'Новосибирск',id: 'nsk'},
-      {label:'Краснодар',id: 'krdr'},
-      {label:'Калининград',id: 'kld'},
-   ]
-})
+setTimeout(function(){
+   clearInterval(interval)
+},7*1000)
