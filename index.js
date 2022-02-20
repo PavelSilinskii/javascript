3d6746cb6bc875df5270ac4e096452ba401e7cbf
Pavel <work322@gmail.com>
2022-02-21 00:03:14 +0300

    promise через async и  await

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-document.querySelector('#load').addEventListener('click',load)
-
-async function load(){
-var url = 'https://jsonplaceholder.typicode.com/users'
-
-var response = await fetch(url)
-var data =await response.json()
-
-var ul = document.querySelector('#list')
-   
-var html = data.map(function(item){
-return `<li>${item.id}  ${item.name} (${item.email})</li>`
-}).join(' ')
-      
-//ul.insertAdjacentHTML('afterbegin',html.join(' '))
-document.querySelector('#list').insertAdjacentHTML('afterbegin',html)
+function sleep(ms){
+   return new Promise(function(resolve){
+      setTimeout(function(){
+         resolve()
+      },ms)
+   })
 }
+
+
+// sleep(1500).then(function(){
+//    console.log('1500')
+// })
+
+// sleep(3000).then(function(){
+//    console.log('3000')
+// })
+
+// Promise.all([sleep(1500), sleep(3000)]).then(function(){ // запускается при последнем promise
+//    console.log('All')
+// })
+
+// Promise.race([sleep(1500), sleep(3000)]).then(function(){ // запускается при первом promise
+//    console.log('Race')
+// })
+
+var p1= sleep(1500).then(function(){
+   return{
+      name: 'Promise 1500'
+   }
+})
+
+var p2= sleep(3000).then(function(){
+   return{
+      name: 'Promise 3000'
+   }
+})
+var p3= sleep(4000).then(function(){
+   return{
+      name: 'Promise 4000'
+   }
+})
+
+async function start(){
+// Promise.all([p1,p2,p3]).then(function(data){
+//    console.log('All',data)
+// })
+
+// Promise.race([p1,p2]).then(function(data){
+//    console.log('All',data)
+// })
+
+var dataAll = await Promise.all([p1, p2, p3])
+var dataRace = await Promise.race([p1, p2])
+console.log('dataAll',dataAll)
+console.log('dataRace',dataRace)
+}
+
+start()
