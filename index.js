d00eb4f6c9ff805394b4633a17b21a0e7840bb66
Pavel <work322@gmail.com>
2022-02-20 23:28:14 +0300

    Запрос к серверу методом fetch

--- index.js
+++ index.js
@@ -1,75 +1,23 @@
-console.log('Клиент: хочу получить список пользователей')
-console.log('...')
+document.querySelector('#load').addEventListener('click',load)
 
-// setTimeout(function(){
-//    console.log('Сервер: запрашиваю список пользователей в БД')
-//    console.log('...')
+function load(){
+var url = 'https://jsonplaceholder.typicode.com/comments'
 
-//    setTimeout(function(){
-//    console.log('БД: формирую список пользователей')
-//    console.log('...')
-//    setTimeout(function(){
-//       console.log('Сервер: трансформирую данные для клиента')
-//       console.log('...')
-//       setTimeout(function(){
-//          console.log('Клиент: получил данные и отображаю их')
-//       },1000)
-//    },500)
-// },500)
-// },1000)
+fetch(url)//запрос к серверу
+   .then(function(response){ //promise
+      return response.json()
+   })
+   .then(function(data){ //promise
+      var ul = document.querySelector('#list')
+   
+      var html = data.map(function(item){
+         return `<li>${item.id}  ${item.name} (${item.email})</li>`
+      })
+      
+      ul.insertAdjacentHTML('afterbegin',html.join(' '))
+      //console.log('Data',html.join(' '))
 
 
-let promise = new Promise(function(resolve,reject){
-setTimeout(function(){
-   console.log('Сервер: запрашиваю список пользователей в БД')
-   console.log('...') 
-   resolve()
-},1000)
-})
-
-promise.then(function(){
-   return new Promise(function(resolve,reject){
-   setTimeout(function(){
-      let users = [
-      {uid: 'id1',name: 'Maxim'},
-      {uid: 'id2',name: 'Elena'},  
-      ]
-     // reject('БД не смогла выполнить запрос') // выброс ошибки
-      console.log('БД: формирую список пользователей')
-      console.log('...')
-   resolve(users)
-   },500)
    })
 
-})
-
-.then(function(dbUsers){
-   return new Promise(function(resolve,reject){
-   setTimeout(function(){
-   console.log('Сервер: трансформирую данные для клиента')
-   console.log('...') 
-   let users = dbUsers.map(function(user){
-      return{
-        id: user.uid,
-        firstName: user.name,
-        timestamp: Date.now() 
-      }
-   })
-   resolve(users)
-   },500)
-   })
-})
-
-.then(function(users){
-   setTimeout(function(){
-      console.log('Клиент: получил данные и отображаю их', users) 
-    },500)
-   })
-
-   .catch(function(error){
-      console.log(error)
-   })
-
-   // .finally(function(){
-   //    console.log('Finally')
-   // })
+}
